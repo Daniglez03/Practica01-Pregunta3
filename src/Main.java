c1e652c48d6757b073dd390c1f0a8900d9fe4d2a
Daniglez03 <gonzalezlorenzodani7@gmail.com>
2022-10-19 13:11:36 +0100

    Feature: Commit final pregunta 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
+import java.io.*;
+
 public class Main {
+    public static void main(String[] args){
+        File file = new File("./data/reader.txt");
+        File fileFichero = new File("./data/fichero.txt");
+        try {
 
-    public static void main(String[] args) {
+            if (!fileFichero.exists()) {
+                System.out.println(fileFichero + " no existe.");
+            }else {
+                // Escribir el resultado del comando hecho en fichero.txt y mostrarlo en reader.txt a través del bash
+                ProcessBuilder builders = new ProcessBuilder(
+                        "C:\\Users\\Dani\\AppData\\Local\\Programs\\Git\\git-bash.exe ",
+                                "-c",
+                                "cat ./data/fichero.txt | wc > ./data/reader.txt");
+                builders.start();
+                FileReader fr = new FileReader(file);
+                BufferedReader br = new BufferedReader(fr);
 
+                String linea;
+                while((linea=br.readLine())!=null) {
+                    System.out.println("Lines:   Words:   Bytes:");
+                    System.out.println(linea);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No existe el fichero.\nCreando " + file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
